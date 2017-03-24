793cd9fd968dc30f3cd520266eb6a439213464a6
Alexander <accounts.alexan059@gmail.com>
2017-03-24 02:05:08 +0100

    Completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,11 +17,19 @@ gulp.task('watch', ['styles'], function() {
 
     watch('./app/assets/styles/**/*.css', function() {
         gulp.start('css-inject');
-    })
+    });
+
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scripts-refresh');
+    });
 
 });
 
 gulp.task('css-inject', ['styles'], function() {
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+});
+
+gulp.task('scripts-refresh', ['scripts'], function() {
+    browserSync.reload();
 });
